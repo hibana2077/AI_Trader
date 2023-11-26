f2012555955cd31d5ff86e83a7467b194d0787f4
hibana2077 <hibana2077@gmail.com>
2023-11-26 20:09:03 +0800

    Update LastEditTime in a2c.py

--- main/test/a2c.py
+++ main/test/a2c.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2023-11-25 13:45:50
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-11-25 17:20:23
+LastEditTime: 2023-11-26 09:33:35
 FilePath: \AI_Trader\main\test\a2c.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
